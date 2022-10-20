a959d7ba6098ee5045fd30d192d0ee094d6b189e
toe <rakinfrhn@gmail.com>
2022-10-20 21:58:43 +0600

    no change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,12 @@
 
 @section('body')
 <section id="hero" class="d-flex align-items-center">
-    <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <h1>Welcome to <span>Matrimony</span></h1>
+    <div class="container"  data-aos-delay="100">
+      <h1>Welcome to <span>Matrimon</span></h1>
       <h2>Matrimony in Bangladesh for finding your life partner</h2>
       <div class="d-flex">
         <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i></a>
       </div>
     </div>
   </section><!-- End Hero -->
