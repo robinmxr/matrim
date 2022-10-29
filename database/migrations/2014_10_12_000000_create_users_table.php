0ad0e219af2c2af61e8fe713f18e62d2a6bea257
Reajul Islam Mojumder Robin <reajulbd786@gmail.com>
2022-10-29 18:34:38 +0600

    payment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,6 +28,7 @@ class CreateUsersTable extends Migration
             $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->string('status')->default('pending');
+            $table->string('payment_status')->default('unpaid');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\SslCommerzPaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -53,6 +54,18 @@ Route::group(['middleware' => 'auth'],function(){
 
 
    });
+
+    Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+    Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+    Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+    Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+    Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+    Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+    Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+    Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
 });
 
 require __DIR__.'/auth.php';
